ea03029188adde3c1c57cab1de3327785423d54b
Michelle Lockett <shelldog42@gmail.com>
2017-10-19 20:19:19 -0700

    Pass functional shared tests for stacks

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newInstance = {};
+  newInstance.storage = {};
+  //extend the methods to the new class
+  _.extend(newInstance, stackMethods);
+
+  return newInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  var index = this.size();
+  this.storage[index] = value;
+}
 
+stackMethods.pop = function() {
+  var top = this.size() - 1;
+  var value = this.storage[top];
+  delete this.storage[top];
+  return value;
+}
+
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+}
