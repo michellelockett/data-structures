a1ee4f2992213c181dc7a5c764075bcdceb8ec31
Michelle Lockett <shelldog42@gmail.com>
2017-10-20 08:15:08 -0700

    Pass tests for prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(queueMethods);
+  newInstance.storage = {};
+  newInstance.key = 0;
+  newInstance.nextInLine = 0;
+  return newInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.key] = value;
+  this.key++;
+}
+
+queueMethods.dequeue = function() {
+  var value = this.storage[this.nextInLine];
+  delete this.storage[this.nextInLine];
+  this.nextInLine++;
+  return value;
+}
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+}
 
