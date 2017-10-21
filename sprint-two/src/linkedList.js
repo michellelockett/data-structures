4aa19f91cd43dcbc1c9ff17b91fb6c883ce18464
Michelle Lockett <shelldog42@gmail.com>
2017-10-21 15:38:34 -0700

    Pass all tests for linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,26 +1,59 @@
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
+  //time complexity constant
+
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else if (list.tail === null) {
+      list.head.next = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
+
+  //time complexity constant
 
   list.removeHead = function() {
+    var originalHead = list.head.value;
+    list.head = list.head.next;
+    return originalHead;
   };
 
-  list.contains = function(target) {
-  };
+  // time complexity O(n) linear
 
+  list.contains = function(target, currentNode) {
+    currentNode = currentNode || list.head;
+    //check to see if the value at the current node is the target
+    //base case value === target, return true
+    if (currentNode.value === target) {
+      return true;
+    }
+    //recursive case
+    if (currentNode.next !== null) {
+      currentNode = currentNode.next;
+      return list.contains(target, currentNode);
+    } else {
+      return false;
+    }
+  };
   return list;
 };
 
+//time complexity constant
 var Node = function(value) {
   var node = {};
 
   node.value = value;
   node.next = null;
-
   return node;
 };
 
