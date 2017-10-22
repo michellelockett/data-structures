151f98f6232eccaab8c4633fa98bb1cc77ce0af9
Michelle Lockett <shelldog42@gmail.com>
2017-10-22 09:29:38 -0700

    Pass tests for graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -8,30 +8,54 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.nodes.includes(node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  var index = this.nodes.indexOf(node);
+  this.nodes.splice(index, 1);
+  for (var i = 0; i < this.edges.length; i++) {
+    if (this.edges[i][0] === node || this.edges[i][1] === node) {
+      this.removeEdge(this.edges[i][0], this.edges[i][1]);
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  for (var i = 0; i < this.edges.length; i++) {
+    if (this.edges[i][0] === fromNode && this.edges[i][1] === toNode) {
+      return true;
+    } else if (this.edges[i][1] === fromNode && this.edges[i][0] === toNode) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges.push([fromNode, toNode]);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  for (var i = 0; i < this.edges.length; i++) {
+    if (this.edges[i][0] === fromNode && this.edges[i][1] === toNode || this.edges[i][0] === toNode && this.edges[i][1] === fromNode) {
+      this.edges.splice(i, 1);
+    }
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  this.nodes.forEach(cb);
 };
 
 /*
