279b370dbef9437f19a52bec724362880987807b
Michelle Lockett <shelldog42@gmail.com>
2017-10-25 23:28:49 -0700

    Rebalancing binary search tree getting stuck in infinite loop

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -5,45 +5,102 @@ describe('binarySearchTree', function() {
     binarySearchTree = BinarySearchTree(5);
   });
 
-  it('should have methods named "insert", "contains", and "depthFirstLog', function() {
-    expect(binarySearchTree.insert).to.be.a('function');
-    expect(binarySearchTree.contains).to.be.a('function');
-    expect(binarySearchTree.depthFirstLog).to.be.a('function');
-  });
+  // it('should have methods named "insert", "contains", and "depthFirstLog', function() {
+  //   expect(binarySearchTree.insert).to.be.a('function');
+  //   expect(binarySearchTree.contains).to.be.a('function');
+  //   expect(binarySearchTree.depthFirstLog).to.be.a('function');
+  // });
 
-  it('should insert values at the correct location in the tree', function() {
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(3);
-    binarySearchTree.insert(7);
-    binarySearchTree.insert(6);
-    expect(binarySearchTree.left.right.value).to.equal(3);
-    expect(binarySearchTree.right.left.value).to.equal(6);
-  });
+  // it('should insert values at the correct location in the tree', function() {
+  //   binarySearchTree.insert(2);
+  //   binarySearchTree.insert(3);
+  //   binarySearchTree.insert(7);
+  //   binarySearchTree.insert(6);
+  //   expect(binarySearchTree.left.right.value).to.equal(3);
+  //   expect(binarySearchTree.right.left.value).to.equal(6);
+  // });
 
-  it('should have a working "contains" method', function() {
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(3);
-    binarySearchTree.insert(7);
-    expect(binarySearchTree.contains(7)).to.equal(true);
-    expect(binarySearchTree.contains(8)).to.equal(false);
-  });
+  // it('should have a working "contains" method', function() {
+  //   binarySearchTree.insert(2);
+  //   binarySearchTree.insert(3);
+  //   binarySearchTree.insert(7);
+  //   expect(binarySearchTree.contains(7)).to.equal(true);
+  //   expect(binarySearchTree.contains(8)).to.equal(false);
+  // });
 
-  it('should execute a callback on every value in a tree using "depthFirstLog"', function() {
-    var array = [];
-    var func = function(value) { array.push(value); };
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(3);
-    binarySearchTree.insert(7);
-    binarySearchTree.depthFirstLog(func);
-    expect(array).to.eql([5, 2, 3, 7]);
-  });
+  // it('should execute a callback on every value in a tree using "depthFirstLog"', function() {
+  //   var array = [];
+  //   var func = function(tree) { array.push(tree.value); };
+  //   binarySearchTree.insert(2);
+  //   binarySearchTree.insert(3);
+  //   binarySearchTree.insert(7);
+  //   binarySearchTree.depthFirstLog(func, binarySearchTree);
+  //   expect(array).to.eql([5, 2, 3, 7]);
+  // });
 
-  it('should handle negative numbers as the root value', function() {
-    binarySearchTree = BinarySearchTree(-5);
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(3);
-    binarySearchTree.insert(7);
-    expect(binarySearchTree.left).to.equal(null);
-    expect(binarySearchTree.right).to.not.equal(null);
+  // it('should handle negative numbers as the root value', function() {
+  //   binarySearchTree = BinarySearchTree(-5);
+  //   binarySearchTree.insert(2);
+  //   binarySearchTree.insert(3);
+  //   binarySearchTree.insert(7);
+  //   expect(binarySearchTree.left).to.equal(null);
+  //   expect(binarySearchTree.right).to.not.equal(null);
+  // });
+
+  // it('should keep a lineage of parent to children', function() {
+  //   binarySearchTree = BinarySearchTree(5);
+  //   binarySearchTree.insert(2);
+  //   binarySearchTree.insert(3);
+  //   binarySearchTree.insert(7);
+  //   expect(binarySearchTree.left.parent.value).to.equal(5);
+  //   expect(binarySearchTree.left.right.parent.value).to.equal(2);
+  //   expect(binarySearchTree.right.parent.value).to.equal(5);
+  // });
+
+  // it('should keep a depth property for every new node', function() {
+  //   binarySearchTree = BinarySearchTree(5);
+  //   binarySearchTree.insert(2);
+  //   binarySearchTree.insert(3);
+  //   binarySearchTree.insert(7);
+  //   expect(binarySearchTree.depth).to.not.equal(null);
+  //   expect(binarySearchTree.depth).to.equal(0);
+  //   expect(binarySearchTree.left.depth).to.equal(1);
+  //   expect(binarySearchTree.right.depth).to.equal(1);
+  //   expect(binarySearchTree.left.right.depth).to.equal(2);
+  // });
+
+  // it('should find a minimum depth and maximum depth', function() {
+  //   binarySearchTree = BinarySearchTree(5);
+  //   binarySearchTree.insert(2);
+  //   binarySearchTree.insert(1);
+  //   binarySearchTree.insert(3);
+  //   binarySearchTree.insert(4);
+  //   binarySearchTree.insert(7);
+  //   var depth = binarySearchTree.checkDepth();
+  //   expect(depth.min).to.equal(1);
+  //   expect(depth.max).to.equal(3);
+  // });
+
+  it('should rebalance a tree when maximum depth is twice minimum depth', function() {
+    binarySearchTree = BinarySearchTree(50);
+    binarySearchTree.insert(20);
+    binarySearchTree.insert(10);
+    binarySearchTree.insert(30);
+    binarySearchTree.insert(40);
+    binarySearchTree.insert(70);
+    //binarySearchTree.insert(2);
+    //binarySearchTree.insert(8);
+    //sbinarySearchTree.insert(81);
+    // binarySearchTree.insert(99);
+    // binarySearchTree.insert(70);
+    // binarySearchTree.insert(75);
+    // binarySearchTree.insert(76);
+    // binarySearchTree.insert(77);
+    // binarySearchTree.insert(78);
+    // binarySearchTree.insert(79);
+    //binarySearchTree.rebalance();
+    //var depth = binarySearchTree.checkDepth();
+    //expect(depth.max).to.equal(3);
+    //expect(depth.min).to.equal(2);
   });
 });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,3 +1,20 @@
+//conditions for when a rebalancing is necessary
+  //when max depth is more than 2 times the min depth
+
+
+//rebalancing
+
+// we need to store an array with all values --> array of nodes
+// we need to have access to the length of the left side of root tree
+// access to the length of the right side of the tree
+
+   //          root
+   //      10         20
+   //    8    11    25    19
+   //  7  9       x    x      x
+   // 4 7.1                      x
+   //                                x
+
 //time complexity constant
 var BinarySearchTree = function(value) {
   // create a root
@@ -6,9 +23,8 @@ var BinarySearchTree = function(value) {
   newTree.value = value;
   newTree.right = null;
   newTree.left = null;
-
-  // keep track of nodes in order to balance
-  newTree.children = [];
+  newTree.parent = null;
+  newTree.depth = 0;
 
   // deal with balance
   return newTree;
@@ -25,21 +41,30 @@ var extend = function(obj1, obj2) {
 
 //time complexity O(logn)
 newTreeMethods.insert = function(value) {
-
+  var newTree = BinarySearchTree(value);
   if (value < this.value) {
     if (this.left === null) {
-      this.left = BinarySearchTree(value);
+      this.left = newTree;
+      newTree.parent = this;
+      newTree.depth = this.depth + 1;
+      this.root = true;
     } else {
       this.left.insert(value);
     }
   } else if (value > this.value) {
     if (this.right === null) {
-      this.right = BinarySearchTree(value);
+      this.right = newTree;
+      newTree.parent = this;
+      newTree.depth = this.depth + 1;
+      this.root = true;
     } else {
       this.right.insert(value);
     }
   }
+
+  this.checkDepth();
 };
+
 //time complexity O(logn)
 newTreeMethods.contains = function(value) {
   // base case
@@ -55,25 +80,106 @@ newTreeMethods.contains = function(value) {
   }
   return false;
 };
-//time complexity n, additional time complexity would depend on the complexity of the callback function
-newTreeMethods.depthFirstLog = function(callback) {
 
-  var traverseTree = function(tree) {
-    callback(tree.value);
-    //base case
+//time complexity n, additional time complexity would depend on the complexity of the callback function
+newTreeMethods.depthFirstLog = function(callback, tree) {
+  callback(tree);
+  //base case
+  if (tree.left === null && tree.right === null) {
+    return;
+  } else {
+    if (tree.left) {
+      this.depthFirstLog(callback, tree.left);
+    }
+    if (tree.right) {
+      this.depthFirstLog(callback, tree.right);
+    }
+  }
+};
+
+newTreeMethods.checkDepth = function() {
+  var depth = {
+    max: 0,
+    min: 0
+  }
+
+  var checkMax = function(tree) {
+    if (tree.depth > depth.max) {
+      depth.max = tree.depth;
+    };
+  };
+
+  var localMin = 10000;
+
+  var checkMin = function(tree) {
+
+    console.log("depth.min = ", depth.min);
     if (tree.left === null && tree.right === null) {
-      return;
-    } else {
-      if (tree.left) {
-        traverseTree(tree.left);
-      }
-      if (tree.right) {
-        traverseTree(tree.right);
+      if (tree.depth < localMin) {
+        console.log('found a leaf');
+        depth.min = tree.depth;
+        localMin = tree.depth;
       }
     }
   };
-  return traverseTree(this);
+
+  this.depthFirstLog(checkMin, this);
+  this.depthFirstLog(checkMax, this);
+  console.log(depth);
+
+  var doubleMin = depth.min * 2;
+  if (depth.max > doubleMin && depth.min !== 0) {
+    //this.rebalance();
+    console.log('say hello');
+  }
+
+}
+
+newTreeMethods.rebalance = function() {
+  var tree = this;
+  var values = [];
+  var getNodes = function(tree) {
+    values.push(tree.value);
+  }
+  this.depthFirstLog(getNodes, this);
+
+  values.sort(function(a, b) {
+    return a - b;
+  });
+
+  var middleOfArray = function(array) {
+    var middle = Math.floor(array.length / 2);
+    return {
+      value: array[middle],
+      left: array.slice(0, middle),
+      right: array.slice(middle + 1)
+    };
+  };
+
+  var valuesObject = middleOfArray(values);
+  var newTree = BinarySearchTree(valuesObject.value);
+
+  var recursiveRebalance = function(array) {
+    //base case left or right array is length 1
+    if (array.length === 1) {
+      newTree.insert(array[0]);
+      return;
+    } else {
+      var newArray = middleOfArray(array);
+      if (newTree.value !== newArray.value) {
+        newTree.insert(newArray.value);
+      }
+      recursiveRebalance(newArray.right);
+      recursiveRebalance(newArray.left);
+    }
+  }
+
+  recursiveRebalance(values);
+
+  return newTree;
 };
+
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
